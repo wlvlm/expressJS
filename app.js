1692445acbbabf1cd0fda45743a823b57ece28ba
William Vuillaume <williampgm77@gmail.com>
2023-11-29 03:42:05 +0100

    Endpoint exercices

---
+++ app.js
@@ -0,0 +1,62 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+const arrNames = [
+    {
+
+        id: 12,
+        name: 'William',
+        age: '22'
+
+    }, {
+
+        id: 15,
+        name: 'Paul',
+        age: '35'
+
+    }, {
+
+        id: 6,
+        name: 'Mathieu',
+        age: '19'
+
+    }
+]
+
+app.get('/', (req, res) => {
+    res.send(`<h1>Page d'accueil</h1><br>
+    <a href="/about">A propos</a>`)
+})
+
+app.get('/about', (req, res) => {
+    res.send(`<h1>A propos</h1><br>
+    <a href="/">Accueil</a>`)
+})
+
+app.get('/names', (req, res) => {
+    let sentence = ''
+    arrNames.forEach((name) => {
+        sentence += name + " "
+    })
+
+    res.send(sentence)
+})
+
+app.get('/names/:id', (req, res) => {
+    // console.log(arrNames[req.params.id].name)
+    let result = "not found";
+    const urlId = parseInt(req.params.id)
+
+    arrNames.forEach((obj, index) => {
+        if(obj.id === urlId){
+            result = arrNames[index].name
+        }
+    })
+
+    res.send(result)
+})
+
+app.listen(port, ()=>{
+    console.log(`L' app d'exemple écoute le port ${port}`)
+})
